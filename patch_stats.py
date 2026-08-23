47853e63514005b1027900c8799ae3205103692a
M. Irfan Rusydi <67940812+rusycem@users.noreply.github.com>
2026-08-24 02:23:25 +0800

    remove py files

--- patch_stats.py
+++
@@ -1,7 +0,0 @@
-with open(r'd:\GodotProjects\merdeka-jam\MerdekaJam2026\addons\semak\core\vn_tree_inspector_builders.gd', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('var stats = ["STR", "DEX", "CON", "INT", "WIS", "CHA"]', 'var stats = ["Charm", "Intelligence", "Courage", "Dexterity"]')
-
-with open(r'd:\GodotProjects\merdeka-jam\MerdekaJam2026\addons\semak\core\vn_tree_inspector_builders.gd', 'w', encoding='utf-8') as f:
-    f.write(text)
